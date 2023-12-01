f9d8f3365312b3b05666fdad62723245b149609e
Nathan Lo <lon12@mcmaster.ca>
2023-12-01 03:16:09 -0500

    fix: objPosArrayList insertTail function fixed

--- objPosArrayList.cpp
+++ objPosArrayList.cpp
@@ -1,65 +1,48 @@
 #include "objPosArrayList.h"
 
-// Check lecture contents on general purpose array list construction, 
+// Check lecture contents on general purpose array list construction,
 // and modify it to support objPos array list construction.
 
-objPosArrayList::objPosArrayList()
-{
+objPosArrayList::objPosArrayList() {
   sizeList = 0;
   sizeArray = ARRAY_MAX_CAP;
   aList = new objPos[sizeArray];
 }
 
-objPosArrayList::~objPosArrayList()
-{
-  delete[] aList;
-}
+objPosArrayList::~objPosArrayList() { delete[] aList; }
 
-int objPosArrayList::getSize()
-{
-  return sizeList;
-}
+int objPosArrayList::getSize() { return sizeList; }
 
-void objPosArrayList::insertHead(objPos thisPos)
-{
-  for (int i = sizeList; i > 0; i--)
-  {
+void objPosArrayList::insertHead(objPos thisPos) {
+  for (int i = sizeList; i > 0; i--) {
     aList[i] = aList[i - 1];
   }
   aList[0] = thisPos;
   sizeList++;
 }
 
-void objPosArrayList::insertTail(objPos thisPos)
-{
+void objPosArrayList::insertTail(objPos thisPos) {
   aList[sizeList++] = thisPos;
+  sizeList++;
 }
 
-void objPosArrayList::removeHead()
-{
-  for (int i = 0; i < sizeList; i++)
-  {
+void objPosArrayList::removeHead() {
+  for (int i = 0; i < sizeList; i++) {
     aList[i] = aList[i + 1];
   }
   sizeList--;
 }
 
-void objPosArrayList::removeTail()
-{
-  sizeList--;
-}
+void objPosArrayList::removeTail() { sizeList--; }
 
-void objPosArrayList::getHeadElement(objPos &returnPos)
-{
+void objPosArrayList::getHeadElement(objPos &returnPos) {
   returnPos = aList[0];
 }
 
-void objPosArrayList::getTailElement(objPos &returnPos)
-{
+void objPosArrayList::getTailElement(objPos &returnPos) {
   returnPos = aList[sizeList - 1];
 }
 
-void objPosArrayList::getElement(objPos &returnPos, int index)
-{
+void objPosArrayList::getElement(objPos &returnPos, int index) {
   returnPos = aList[index];
 }
